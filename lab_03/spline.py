c29d576c27e82a0a4173df4fc34f4eda004f34d1
mmd19u540 <mymidi.student@yandex.ru>
2021-04-12 21:31:47 +0300

    ADD: count of c coefs

---
+++ lab_03/spline.py
@@ -0,0 +1,82 @@
+"""
+    Модуль сплайн-интерполяции табличных функций
+"""
+
+def find_h_coefs(func):
+    """
+        Поиск коэффициентов h
+    """
+    h_coefs = [0.]
+
+    for i in range(1, len(func)):
+        h_coefs.append(func[i][0] - func[i - 1][0])
+
+    return h_coefs
+
+
+def find_f_coefs(func, h_coefs):
+    """
+        Поиск коэффициентов f
+    """
+    f_coefs = [0., 0.]
+
+    for i in range(2, len(func)):
+        dy1 = func[i][1] - func[i - 1][1]
+        dy2 = func[i - 1][1] - func[i - 2][1]
+        f_coefs.append(3 * (dy1 / h_coefs[i] - dy2 / h_coefs[i - 1]))
+
+    return f_coefs
+
+
+def find_ksi_coefs(func, h):
+    """
+        Поиск коэффициентов ξ
+    """
+    ksi_coefs = [0., 0., 0.]
+
+    for i in range(3, len(func)):
+        cur_ksi = - h[i - 1] / (h[i - 2] * ksi_coefs[i - 1] + 2 * (h[i - 2] + h[i - 1]))
+        ksi_coefs.append(cur_ksi)
+
+    return ksi_coefs
+
+
+def find_eta_coefs(func, ksi, h, f):
+    """
+        Поиск коэффициентов η
+    """
+    eta_coefs = [0., 0., 0.]
+
+    for i in range(3, len(func)):
+        cur_eta = (f[i - 1] - h[i - 2] * eta_coefs[i - 1]) / (h[i - 2] * ksi[i - 1] + 2 * (h[i - 2] + h[i - 1]))
+        eta_coefs.append(cur_eta)
+
+    return eta_coefs
+
+
+def find_c_coefs(ksi, eta):
+    """
+        Подсчет коэффициентов с
+    """
+    num = len(eta)
+    c_coefs = [eta[num - 1]]
+
+
+    for i in range(num - 1, 1, -1):
+        print(c_coefs)
+        c_coefs.insert(0, ksi[i] * c_coefs[0] + eta[i])
+
+    return c_coefs
+
+
+def get_c_coefs(func, arg):
+    """
+        Поиск коффициентов при второй степени сплайна
+    """
+    h_coefs = find_h_coefs(func)
+    ksi_coefs = find_ksi_coefs(func, h_coefs)
+    f_coefs = find_f_coefs(func, h_coefs)
+    eta_coefs = find_eta_coefs(func, ksi_coefs, h_coefs, f_coefs)
+    c_coefs = find_c_coefs(ksi_coefs, eta_coefs)
+
+    return c_coefs
