72b068a4651bbeee5e5ebaa5ec5a031cc2e0f58a
mmd19u540 <mymidi.student@yandex.ru>
2021-04-13 13:30:55 +0300

    ADD: getting b and d coefs

--- lab_03/spline.py
+++ lab_03/spline.py
@@ -6,7 +6,7 @@ def find_h_coefs(func):
     """
         Поиск коэффициентов h
     """
-    h_coefs = [0.]
+    h_coefs = []
 
     for i in range(1, len(func)):
         h_coefs.append(func[i][0] - func[i - 1][0])
@@ -14,16 +14,16 @@ def find_h_coefs(func):
     return h_coefs
 
 
-def find_f_coefs(func, h_coefs):
+def find_f_coefs(func, h):
     """
         Поиск коэффициентов f
     """
-    f_coefs = [0., 0.]
+    f_coefs = [0.]
 
     for i in range(2, len(func)):
         dy1 = func[i][1] - func[i - 1][1]
         dy2 = func[i - 1][1] - func[i - 2][1]
-        f_coefs.append(3 * (dy1 / h_coefs[i] - dy2 / h_coefs[i - 1]))
+        f_coefs.append(3 * (dy1 / h[i - 1] - dy2 / h[i - 2]))
 
     return f_coefs
 
@@ -32,9 +32,9 @@ def find_ksi_coefs(func, h):
     """
         Поиск коэффициентов ξ
     """
-    ksi_coefs = [0., 0., 0.]
+    ksi_coefs = [0., 0.]
 
-    for i in range(3, len(func)):
+    for i in range(2, len(func)):
         cur_ksi = - h[i - 1] / (h[i - 2] * ksi_coefs[i - 1] + 2 * (h[i - 2] + h[i - 1]))
         ksi_coefs.append(cur_ksi)
 
@@ -45,10 +45,11 @@ def find_eta_coefs(func, ksi, h, f):
     """
         Поиск коэффициентов η
     """
-    eta_coefs = [0., 0., 0.]
+    eta_coefs = [0., 0.]
 
-    for i in range(3, len(func)):
+    for i in range(2, len(func)):
         cur_eta = (f[i - 1] - h[i - 2] * eta_coefs[i - 1]) / (h[i - 2] * ksi[i - 1] + 2 * (h[i - 2] + h[i - 1]))
+        print("cur eta", cur_eta)
         eta_coefs.append(cur_eta)
 
     return eta_coefs
@@ -59,21 +60,24 @@ def find_c_coefs(ksi, eta):
         Подсчет коэффициентов с
     """
     num = len(eta)
-    c_coefs = [eta[num - 1]]
-
+    c_coefs = [0]
+    print("ksi:")
+    for k in ksi:
+        print("{:.6f}".format(k))
+    print("eta:")
+    for e in eta:
+        print("{:.6f}".format(e))
 
     for i in range(num - 1, 1, -1):
-        print(c_coefs)
         c_coefs.insert(0, ksi[i] * c_coefs[0] + eta[i])
 
     return c_coefs
 
 
-def get_c_coefs(func, arg):
+def get_c_coefs(func, h_coefs):
     """
         Поиск коффициентов при второй степени сплайна
     """
-    h_coefs = find_h_coefs(func)
     ksi_coefs = find_ksi_coefs(func, h_coefs)
     f_coefs = find_f_coefs(func, h_coefs)
     eta_coefs = find_eta_coefs(func, ksi_coefs, h_coefs, f_coefs)
@@ -92,15 +96,42 @@ def get_x_position(func, x):
     while x > func[pos][0]:
         pos += 1
 
-    pos -= 1
-
-    if pos < 0:
-        pos = 0
-
     if pos > len(func) - 1:
         pos = len(func) - 1
 
     return pos
+
+
+def get_b_coefs(func, h, c):
+    """
+        Подсчет коэффициентов b
+    """
+    b_coefs = [0.]
+    N = len(func) - 1
+
+    for i in range(1, N):
+        cur_b = (func[i][1] - func[i - 1][1]) / h[i] - h[i] * (c[i + 1] - 2 * c[i]) / 3
+        b_coefs.append(cur_b)
+
+    b_coefs.append((func[N][1] - func[N - 1][1]) / h[N] - h[N] * 2 * c[N] / 3)
+
+    return b_coefs
+
+
+def get_d_coefs(h, c):
+    """
+        Подсчет коэффициентов b
+    """
+    d_coefs = [0.]
+    N = len(h) - 1
+
+    for i in range(1, N):
+        cur_d = (c[i + 1] - c[i]) / (3 * h[i])
+        d_coefs.append(cur_d)
+
+    d_coefs.append(- c[N] / (3 * h[N]))
+
+    return d_coefs
 
 
 def count_spline(func, x):
@@ -109,5 +140,39 @@ def count_spline(func, x):
     """
     position = get_x_position(func, x)
 
-    a_coef = func[position][1]
-    return a_coef
+    a_coefs = [point[1] for point in func]
+
+    print("a:")
+    for a in a_coefs:
+        print("{:.6f}".format(a))
+
+    h_coefs = find_h_coefs(func)
+
+    print("h:")
+    for h in h_coefs:
+        print("{:.6f}".format(h))
+
+    c_coefs = get_c_coefs(func, h_coefs)
+
+    print("c:")
+    for c in c_coefs:
+        print("{:.6f}".format(c))
+
+    b_coefs = get_b_coefs(func, h_coefs, c_coefs)
+
+    print("b:")
+    for b in b_coefs:
+        print("{:.6f}".format(b))
+
+    d_coefs = get_d_coefs(h_coefs, c_coefs)
+
+    print("d:")
+    for d in d_coefs:
+        print("{:.6f}".format(d))
+    print()
+
+    dif = x - func[position][0]
+
+    answer = a_coefs[position] + b_coefs[position] * dif + c_coefs[position] * dif ** 2 + d_coefs[position] * dif ** 3
+
+    return answer
