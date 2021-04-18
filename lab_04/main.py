b9f5419cc3bc8027d0970e2a8360ae57af5eacc8
mmd19u540 <mymidi.student@yandex.ru>
2021-04-18 10:01:45 +0300

    FIX: way to load ui

--- lab_04/main.py
+++ lab_04/main.py
@@ -3,13 +3,14 @@ from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QHeaderView
 import matplotlib.pyplot as plt
 
+from MainWindow import Ui_MainWindow
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
 
-        uic.loadUi('mainwindow.ui', self)
         self.plotBtn.clicked.connect(generate)
 
         self.pointsTable.setColumnCount(3)
@@ -17,7 +18,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.pointsTable.setItem(0, 0, QTableWidgetItem("0.9989"))
         self.pointsTable.setItem(0, 1, QTableWidgetItem("Text in column 2"))
         self.pointsTable.setItem(0, 2, QTableWidgetItem("Text in column 3"))
-        self.pointsTable.horizontalHeader().setSectionResizeMode(2,QtWidgets.QHeaderView.Stretch)
+        self.pointsTable.horizontalHeader().setSectionResizeMode(2, QHeaderView.Stretch)
         print(self.pointsTable.item(0,0).text())
 
 
@@ -28,7 +29,6 @@ def generate():
 
 if __name__ == '__main__': 
     app = QtWidgets.QApplication(sys.argv)
-
     main = MainWindow()
     main.show()
     sys.exit(app.exec_())
