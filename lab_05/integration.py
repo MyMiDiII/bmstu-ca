716b99d19c219910103b97a70f6390d542c5ac83
mmd19u540 <mymidi.student@yandex.ru>
2021-05-02 17:30:20 +0300

    ADD: simpson and gauss integration

---
+++ lab_05/integration.py
@@ -0,0 +1,50 @@
+"""
+    Модуль численного интегрирования
+"""
+
+from numpy.polynomial.legendre import leggauss
+
+def simpson(func, left, right, num):
+    """
+        Интегрирование методом Симпсона
+    """
+    step = (right - left) / num
+
+    result = 0
+
+    x = left
+    
+    while x < right:
+        xNext = x + step
+        result += func(x) + 4 * func(x + step / 2)  + func(xNext)
+        x = xNext
+
+    return step / 6 * result
+
+
+def toX(t, left, right):
+    """
+        Преобразование переменной
+    """
+    return (right + left) / 2 + (right - left) / 2 * t
+
+
+def gauss(func, left, right, num):
+    """
+        Интегрирование методом Гаусса
+    """
+    nodes, coefs = leggauss(num)
+
+    result = 0
+
+    for i in range(num):
+        result += coefs[i] * func(toX(nodes[i], left, right))
+
+    return (right - left) / 2 * result
+
+
+def getTauFunc(xNum, yNum):
+    """
+        Получение функции, зависящей только от tau
+    """
+    return lambda tau: tau ** 2
